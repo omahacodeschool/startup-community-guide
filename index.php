9017adc13c4aa9334135e181a70b9599d698485d
Skyler Sidner <malevolentdragon@gmail.com>
2015-04-05 13:52:21 -0500

    moved content for infographic from index to content file;

--- index.php
+++ index.php
@@ -26,6 +26,9 @@ get_header();
       elseif ($template_part == 'page-map.php') {
         get_template_part('content', 'map');
       }
+      elseif ($template_part == 'page-infographic.php') {
+        get_template_part('content', 'infographic');
+      }
     endwhile;
    endif;
 
@@ -33,29 +36,7 @@ get_header();
   
   <!--START ABOUT OMAHA-->
 
-  <div class="chevron-section" id="about"> 
-    <div class="chevron">
-      <div class="row">
-        <div class="column small-12 medium-6 large-6 top-margin-push">
-          <div class="large-text">
-            <h1 class="bigfont rotate line-height-scrunch">About Omaha</h1>
-          </div> <!-- end large text -->
-        
-        </div><!--end column one-->
-      
-        <div class="column small-12 medium-6 large-6 top-margin-push">
-          <div class="circle centered">
-            <a href="#"> <p class="circle-text"><i class="fa fa-line-chart grey-icon"></i></p></a>
-          </div><!--end circle-image-->
-        </div><!--end column one-->
-   
-      </div>
-    </div>
-  </div><!--end fourth row-->
-  
-  <div class="gray">
-    <!-- <img src="images/WhiteBottomBanner.svg" class="width-100" border="1px"> -->
-  </div><!--end triangle-->
+
   
   <!--START MAP-->
   
