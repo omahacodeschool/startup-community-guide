fe92e807ff09ca63d77c1c0f3a3923d83e5c3b88
Skyler Sidner <malevolentdragon@gmail.com>
2015-04-05 14:04:05 -0500

    fixed move of hard content to content, not page;

---
+++ content-infographic.php
@@ -0,0 +1,23 @@
+  <div class="chevron-section" id="about"> 
+    <div class="chevron">
+      <div class="row">
+        <div class="column small-12 medium-6 large-6 top-margin-push">
+          <div class="large-text">
+            <h1 class="bigfont rotate line-height-scrunch">About Omaha</h1>
+          </div> <!-- end large text -->
+        
+        </div><!--end column one-->
+      
+        <div class="column small-12 medium-6 large-6 top-margin-push">
+          <div class="circle centered">
+            <a href="#"> <p class="circle-text"><i class="fa fa-line-chart grey-icon"></i></p></a>
+          </div><!--end circle-image-->
+        </div><!--end column one-->
+   
+      </div>
+    </div>
+  </div><!--end fourth row-->
+  
+  <div class="gray">
+    <!-- <img src="images/WhiteBottomBanner.svg" class="width-100" border="1px"> -->
+  </div><!--end triangle-->
