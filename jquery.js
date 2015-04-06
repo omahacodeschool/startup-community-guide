2114df3ad8bae505444b48d4169d0ac947a8738b
CharDale Barnes <differentaccounts@hotmail.com>
2015-04-05 23:07:46 -0500

    Updated jquery file, php files and styles to match local version.

--- jquery.js
+++ jquery.js
@@ -24,11 +24,30 @@ $(document).ready(function(){
         });
     });
     
-    $('nav a').click(function(){
+    $(function () { 
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 475) {
+                $('#jsNav').fadeIn();
+            } else {
+                $('#jsNav').fadeOut();
+            }
+        });
+
+        // scroll body to 0px on click
+        $('.go-top a').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 1000);
+            return false;
+        });
+    });
+    
+    $('a').click(function(){
         $('html, body').animate({
             scrollTop: $( $.attr(this, 'href') ).offset().top
         }, 900);
         return false;
     });
+    
 
 });
